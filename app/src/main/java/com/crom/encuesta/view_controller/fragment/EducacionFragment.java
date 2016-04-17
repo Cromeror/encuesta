86b4f1b15e14437f5c66756ce7b3755e38b332f9
Cristobal Romero <cristobalromerorossi@gmail.com>
2016-04-16 23:05:54 -0500

    add: validacion pregunta 2 y 3

--- app/src/main/java/com/crom/encuesta/view_controller/fragment/EducacionFragment.java
+++ app/src/main/java/com/crom/encuesta/view_controller/fragment/EducacionFragment.java
@@ -2,36 +2,71 @@ package com.crom.encuesta.view_controller.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.crom.encuesta.R;
+import com.crom.encuesta.view_controller.subFragment.EstablecimientoEducativoSubFragment;
 
 /**
  * Created by Katherine Buelvas on 15/04/16.
  */
 public class EducacionFragment extends Fragment {
+    private EstablecimientoEducativoSubFragment establecimiento;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_educacion, container, false);
-
+        establecimiento = new EstablecimientoEducativoSubFragment();
+        Switch estudia = (Switch) view.findViewById(R.id.estudia);
+        estudia.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked==true){
+                    getFragmentManager().beginTransaction().replace(R.id.content_establecimiento,establecimiento).commit();
+                }else {
+                    getFragmentManager().beginTransaction().remove(establecimiento).commit();
+                }
+            }
+        });
         Spinner nivelEducativo = (Spinner) view.findViewById(R.id.nivel_educativo);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
                 R.array.nivel_educativo_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         nivelEducativo.setAdapter(adapter);
-
-        Spinner tituloMayor = (Spinner) view.findViewById(R.id.mayor_titulo_obtenido);
+        final Spinner tituloMayor = (Spinner) view.findViewById(R.id.mayor_titulo_obtenido);
         ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(getActivity(),
                 R.array.titulo_educativo_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         tituloMayor.setAdapter(adapter2);
+
+        nivelEducativo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                if (position <= 3 || position == 6) {
+                    tituloMayor.setEnabled(false);
+                    tituloMayor.setClickable(false);
+                    tituloMayor.setSelection(5);
+                } else {
+                    tituloMayor.setEnabled(true);
+                    tituloMayor.setClickable(true);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+            }
+
+        });
 
         return view;
     }
