21ad9d8ade282762004496c6087651ee0c659e80
Cristobal Romero <cristobalromerorossi@gmail.com>
2016-05-24 18:26:29 -0500

    Integracion de vistas

--- app/src/main/java/com/crom/encuesta/view_controller/ViviendaHogarActivity.java
+++ app/src/main/java/com/crom/encuesta/view_controller/ViviendaHogarActivity.java
@@ -1,22 +1,28 @@
 package com.crom.encuesta.view_controller;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.KeyEvent;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.crom.encuesta.R;
-import com.crom.encuesta.view_controller.subFragment.ListaEdadSubFragment;
+import com.crom.encuesta.view_controller.custom.DialogPersonasXCuarto;
+import com.crom.encuesta.view_controller.fragment.CualOtroFragment;
 
 public class ViviendaHogarActivity extends AppCompatActivity {
 
+    CualOtroFragment otro = new CualOtroFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,8 +30,8 @@ public class ViviendaHogarActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         //Spinners
-        Spinner tipoVivienda = (Spinner) findViewById(R.id.tipo_vivienda);
-        Spinner viviendaDondeHabita = (Spinner) findViewById(R.id.vivienda_habita_es);
+        final Spinner tipoVivienda = (Spinner) findViewById(R.id.tipo_vivienda);
+        final Spinner viviendaDondeHabita = (Spinner) findViewById(R.id.vivienda_habita_es);
         Spinner materialPiso = (Spinner) findViewById(R.id.material_pisos);
         Spinner materialExteriores = (Spinner) findViewById(R.id.material_exteriores);
         Spinner serviciosVivienda = (Spinner) findViewById(R.id.servicios_vivienda);
@@ -36,32 +42,83 @@ public class ViviendaHogarActivity extends AppCompatActivity {
         Spinner sanitarioUtilizaVivienda = (Spinner) findViewById(R.id.servicio_sanitario_viviendas);
         Spinner energiaCocina = (Spinner) findViewById(R.id.energia_cocina);
         Spinner serviciosBienesHogar = (Spinner) findViewById(R.id.servicios_bienes_hogar);
+
         //Otros componentes
         EditText mensualidadArriendo = (EditText) findViewById(R.id.mensualidad_arriendo);
         EditText cuartosVivienda = (EditText) findViewById(R.id.cuartos_vivienda);
-        EditText personasXCuarto = (EditText) findViewById(R.id.personas_cuarto_vivienda);
+        TextView personasXCuarto = (TextView) findViewById(R.id.personas_cuarto_vivienda);
         //Configuraciones Spinner
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.tipo_vivienda__array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.tipo_vivienda__array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,R.array.vivienda_habita_es_array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,R.array.material_pisos_array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this,R.array.material_exteriores_array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter5 = ArrayAdapter.createFromResource(this,R.array.servicios_vivienda_array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter6 = ArrayAdapter.createFromResource(this,R.array.agua_array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter7 = ArrayAdapter.createFromResource(this,R.array.agua_array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter8 = ArrayAdapter.createFromResource(this,R.array.como_eliminan_basura_array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter9 = ArrayAdapter.createFromResource(this,R.array.servicio_sanitario_hogar_array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter10 = ArrayAdapter.createFromResource(this,R.array.servicio_sanitario_viviendas_array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter11 = ArrayAdapter.createFromResource(this,R.array.energia_cocina_array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter12 = ArrayAdapter.createFromResource(this,R.array.servicios_bienes_hogar_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter6.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter7.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter8.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter9.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter10.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter11.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter12.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         tipoVivienda.setAdapter(adapter);
-       ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
-                R.array.vivienda_habita_es_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         viviendaDondeHabita.setAdapter(adapter2);
-       /* ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
-                R.array.nivel_educativo_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        nivelEducativo.setAdapter(adapter);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
-                R.array.nivel_educativo_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        nivelEducativo.setAdapter(adapter);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
-                R.array.nivel_educativo_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        nivelEducativo.setAdapter(adapter);
-*/
-    }
+        materialPiso.setAdapter(adapter3);
+        materialExteriores.setAdapter(adapter4);
+        serviciosVivienda.setAdapter(adapter5);
+        aguaConsumo.setAdapter(adapter6);
+        aguaOtrasActividades.setAdapter(adapter7);
+        eliminanBasuras.setAdapter(adapter8);
+        servicioSanitarioHogar.setAdapter(adapter9);
+        sanitarioUtilizaVivienda.setAdapter(adapter10);
+        energiaCocina.setAdapter(adapter11);
+        serviciosBienesHogar.setAdapter(adapter12);
+        viviendaDondeHabita.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            FragmentManager fragmentManager = ViviendaHogarActivity.this.getSupportFragmentManager();
+            boolean bandera = false;
 
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                if (position == viviendaDondeHabita.getCount() - 1) {
+                    fragmentManager.beginTransaction().add(R.id.content_otro, otro).commit();
+                    bandera = true;
+                } else if (bandera) {
+                    fragmentManager.beginTransaction().remove(otro).commit();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+            }
+        });
+        personasXCuarto.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                //Log.i("#################", "Focus");
+                DialogPersonasXCuarto dialogPersonasXCuarto = new DialogPersonasXCuarto();
+                dialogPersonasXCuarto.getDialog().show();
+                return false;
+            }
+        });
+        personasXCuarto.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+
+            }
+        });
+    }
+    public void next(View v){
+
+    }
 }
---
+++ app/src/main/java/com/crom/encuesta/view_controller/custom/DialogPersonasXCuarto.java
@@ -0,0 +1,30 @@
+package com.crom.encuesta.view_controller.custom;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+
+import com.crom.encuesta.R;
+
+/**
+ * Created by Katherine Buelvas on 10/05/16.
+ */
+public class DialogPersonasXCuarto extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        builder.setView(inflater.inflate(R.layout.dialog_persona_por_cuarto, null))
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        return builder.create();
+    }
+}
